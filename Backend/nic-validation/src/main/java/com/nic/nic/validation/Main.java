36a1098f47e9126b896c93c44c7d55f414e9e67b
SandaruwanWeerawardhana <werawardanasw@gmail.com>
2025-12-19 23:13:57 +0530

    chore: initialize project setup and commit initial version to repository

---
+++ Backend/nic-validation/src/main/java/com/nic/nic/validation/Main.java
@@ -0,0 +1,14 @@
+package com.nic.nic.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Main.class, args);
+	}
+
+}
