6e55049cb546efd59cece8aa35b3f2da881eced8
SandaruwanWeerawardhana <werawardanasw@gmail.com>
2025-12-20 15:45:31 +0530

    feat: add Validation class Entity

---
+++ Backend/nic-validation/src/main/java/com/nic/nic/validation/entity/ValidationEntity.java
@@ -0,0 +1,32 @@
+package com.nic.nic.validation.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import java.time.LocalDate;
+@Entity
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Table(name = "nic_validation")
+public class ValidationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "NIC is required")
+    @Pattern(
+            regexp = "^(\\d{9}[vVxX]|\\d{12})$",
+            message = "NIC must be 9 digits followed by V/X or 12 digits"
+    )
+    private String nicNumber;
+
+    @NotBlank(message = "DOB is required")
+    private LocalDate dob;
+
+    @NotBlank(message = "Gender is required")
+    private String gender;
+
+
+}
