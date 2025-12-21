d9df7ec552d23090bd91c6726b1c80c462df0fef
SandaruwanWeerawardhana <werawardanasw@gmail.com>
2025-12-21 22:47:12 +0530

    feat: implement validation logic and endpoints for NIC processing

--- Backend/nic-validation/src/main/java/com/nic/nic/validation/repository/ValidationRepository.java
+++ Backend/nic-validation/src/main/java/com/nic/nic/validation/repository/ValidationRepository.java
@@ -3,5 +3,8 @@ package com.nic.nic.validation.repository;
 import com.nic.nic.validation.entity.ValidationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ValidationRepository extends JpaRepository <ValidationEntity, Long> {
+    Optional<ValidationEntity> findByNicNumber(String nicNumber);
 }
--- Backend/nic-validation/src/main/java/com/nic/nic/validation/service/ValidationService.java
+++ Backend/nic-validation/src/main/java/com/nic/nic/validation/service/ValidationService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 
 @Service
@@ -19,10 +21,26 @@ public class ValidationService {
         return repository.save(mapper.map(request, ValidationEntity.class));
     }
 
+    public Validation validateByNic(String nicNumber) {
+        ValidationEntity entity = repository.findByNicNumber(nicNumber)
+                .orElseThrow(() -> new RuntimeException("NIC not found: " + nicNumber));
+        Validation validation = mapper.map(entity, Validation.class);
+        validation.setAge(calculateAgeFromNic(entity.getDob()));
+        return validation;
+    }
+
     public List<Validation> getAll() {
         List<ValidationEntity> entity = repository.findAll();
         return entity.stream()
-                .map(e -> mapper.map(e, Validation.class))
-                .toList();
+                .map(e ->{
+                    Validation validation = mapper.map(e, Validation.class);
+                    validation.setAge(calculateAgeFromNic(e.getDob()));
+                    return validation;
+                }).toList();
     }
+
+    private Integer calculateAgeFromNic(LocalDate dob) {
+        return Period.between(dob, LocalDate.now()).getYears();
+    }
+
 }
