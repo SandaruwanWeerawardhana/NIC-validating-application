c7b654963451045ce6a73f0df4f61e5ebec4d9e8
SandaruwanWeerawardhana <werawardanasw@gmail.com>
2026-01-25 14:47:58 +0530

    feat: add footer to NICValidator page with copyright information

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,11 +3,9 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
 import NICValidator from './pages/NICValidator';
 import AddRecord from './pages/AddRecord';
 import { Layout } from './components/Layout';
-import { ProtectedRoute } from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -17,13 +15,13 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
-        <Route element={<ProtectedRoute />}>
+        {/* <Route element={<ProtectedRoute />}> */}
           <Route element={<Layout />}>
-            <Route path="/" element={<Dashboard />} />
+            <Route path="/" element={<NICValidator />} />
             <Route path="/validate" element={<NICValidator />} />
             <Route path="/add" element={<AddRecord />} />
           </Route>
-        </Route>
+        {/* </Route> */}
 
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
